9c2338913a3d7b45dccb07ff09ef7a060a30c9e4
Amadeus Demarzi <amadeusdemarzi@gmail.com>
2019-05-21 20:12:09 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {PureComponent, Component, createContext} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const CounterContext = createContext(0);
+
+const FuncCounter = () => (
+  <CounterContext.Consumer>
+    {value => {
+      console.log('rendering FuncCounter');
+      return <div>Counter is {value}</div>;
+    }}
+  </CounterContext.Consumer>
+);
+
+const FuncChild = () => {
+  console.log('rendering FuncChild');
+  return <div>Functional child</div>;
+};
+
+class Tester extends PureComponent {
+  render() {
+    console.log('rendering Tester');
+    return (
+      <div>
+        I am a pure child
+        <FuncCounter />
+        <FuncChild />
+      </div>
+    );
+  }
+}
+
+class App extends Component {
+  state = {
+    counter: 0,
+  };
+
+  componentDidMount() {
+    setInterval(() => this.setState(({counter}) => ({counter: counter + 1})), 1000);
+  }
+
+  render() {
+    console.log('rendering App');
+    return (
+      <CounterContext.Provider value={this.state.counter}>
+        <FuncCounter />
+        <Tester />
+        <FuncChild />
+      </CounterContext.Provider>
+    );
+  }
 }
 
 export default App;
